58d05f8081e0aab67765b1d247d9f9d5803367b1
wa-pgeorg <philipp.georg@webacumen.de>
2020-03-14 16:03:42 +0100

    development

--- src/Resources/app/administration/src/module/sw-cms/index.js
+++ src/Resources/app/administration/src/module/sw-cms/index.js
@@ -1,1 +1,13 @@
+const { Module } = Shopware;
+
 import './component';
+import deDE from './snippet/de-DE';
+import enGB from './snippet/en-GB';
+
+Module.register('moorl-cms', {
+    snippets: {
+        'de-DE': deDE,
+        'en-GB': enGB
+    },
+});
+
