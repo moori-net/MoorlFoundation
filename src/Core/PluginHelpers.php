14ccaf0ce001d0ed94c97c00caa2b13b190c3e9f
wa-pgeorg <philipp.georg@webacumen.de>
2020-03-20 22:29:35 +0100

    dev

---
+++ src/Core/PluginHelpers.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MoorlFoundation\Core;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\NotFilter;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Doctrine\DBAL\Connection;
+
+class PluginHelpers
+{
+
+    public static function removeCmsBlocks($container, $context, $types)
+    {
+
+        $repo = $container->get('cms_block.repository');
+
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsAnyFilter('type', $types));
+
+        $result = $repo->searchIds($criteria, $context);
+
+        $ids = array_map(static function ($id) {
+            return ['id' => $id];
+        }, $result->getIds());
+
+        $repo->delete($ids, $context);
+
+    }
+
+    public static function dropTables($container, $context, $tables)
+    {
+
+        $connection = $container->get(Connection::class);
+
+        foreach ($tables as $table) {
+            $connection->executeQuery('DROP TABLE IF EXISTS `' . $table . '`;');
+        }
+
+    }
+
+}
