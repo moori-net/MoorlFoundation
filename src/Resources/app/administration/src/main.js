ce53a7af0d8855bdd1988dc0eaba2ffb122cba44
moori <info@moorleiche.com>
2020-07-24 21:59:51 +0200

    dev

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -2,4 +2,4 @@
 import './core/moorl-foundation'
 import './core/moorl-foundation.scss'
 import './module/sw-cms';
-import './module/moorl-foundation-article'
+//import './module/moorl-foundation-article'
--- src/Resources/app/administration/src/module/sw-cms/index.js
+++ src/Resources/app/administration/src/module/sw-cms/index.js
@@ -1,8 +1,8 @@
 const { Module } = Shopware;
 
 import './component';
-import deDE from './snippet/de-DE';
-import enGB from './snippet/en-GB';
+import deDE from '../../snippet/de-DE';
+import enGB from '../../snippet/en-GB';
 
 Module.register('moorl-cms', {
     snippets: {
