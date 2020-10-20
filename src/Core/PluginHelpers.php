bb69504158259a6e6a6b4d05f068c2765c8fa840
moori <info@moorleiche.com>
2020-10-20 21:40:12 +0200

    dev

--- src/Core/PluginHelpers.php
+++ src/Core/PluginHelpers.php
@@ -11,6 +11,19 @@ use Doctrine\DBAL\Connection;
 
 class PluginHelpers
 {
+    public static function scrambleWord(string $word): string
+    {
+        if (strlen($word) < 2)
+            return $word;
+        else
+            return $word{0} . str_shuffle(substr($word, 1, -1)) . $word{strlen($word) - 1};
+    }
+
+    public static function scrambleText(string $text): string
+    {
+        return preg_replace('/(\w+)/e', 'self::scrambleWord("\1")', $text);
+    }
+
     public function assignArrayByPath(&$arr, $path, $value, $separator='.') {
         $keys = explode($separator, $path);
         foreach ($keys as $key) {
