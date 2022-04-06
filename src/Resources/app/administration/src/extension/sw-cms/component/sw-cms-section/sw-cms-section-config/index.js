16779bf06714e3a6bf6410c9e33de196a342d836
moori <info@moorleiche.com>
2022-04-06 07:22:50 +0200

    [INITIAL]

--- src/Resources/app/administration/src/extension/sw-cms/component/sw-cms-section/sw-cms-section-config/index.js
+++ src/Resources/app/administration/src/extension/sw-cms/component/sw-cms-section/sw-cms-section-config/index.js
@@ -3,5 +3,11 @@ import template from './sw-cms-section-config.html.twig';
 const {Component} = Shopware;
 
 Component.override('sw-cms-section-config', {
-    template
+    template,
+
+    computed: {
+        moorlIsUnlocked() {
+            return Shopware.State.get('moorlFoundationState').unlocked;
+        }
+    }
 });
