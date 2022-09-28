e0e7bc3c744a8c21b659c139692e8f7b5b442314
moori <info@moorleiche.com>
2022-09-28 08:09:54 +0200

    -

--- src/Resources/app/storefront/build/webpack.config.js
+++ src/Resources/app/storefront/build/webpack.config.js
@@ -3,7 +3,8 @@ module.exports = function (params) {
     return {
         resolve: {
             modules: [
-                `${params.basePath}Resources/node_modules/`
+                `${params.basePath}Resources/node_modules/`,
+                `${params.basePath}Resources/app/administration/node_modules/`
             ],
             alias: {
                 MoorlFoundation: `${params.basePath}Resources/app/storefront/src/`,
