e153011b0cf1ef8985bbcac19444d8d60fde08ef
moorl <info@moorleiche.com>
2023-02-23 11:51:42 +0100

    file explorer

--- src/Core/Content/Client/ClientAwsS3.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace MoorlFoundation\Core\Content\Client;
-
-class ClientAwsS3 extends ClientExtension implements ClientInterface
-{
-    protected string $clientName = "aws-s3";
-}
--- src/Core/Content/Client/ClientGoogleCloudStorage.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace MoorlFoundation\Core\Content\Client;
-
-class ClientGoogleCloudStorage extends ClientExtension implements ClientInterface
-{
-    protected string $clientName = "google-cloud-storage";
-}
