30b3d79f74ba929c80fcb4066aae1bc4fddd83d3
moorl <info@moorleiche.com>
2023-07-07 11:53:04 +0200

    open street map cookie optional

--- src/Core/Service/LocationCookieProvider.php
+++ src/Core/Service/LocationCookieProvider.php
@@ -2,17 +2,26 @@
 
 namespace MoorlFoundation\Core\Service;
 
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Storefront\Framework\Cookie\CookieProviderInterface;
 
 class LocationCookieProvider implements CookieProviderInterface
 {
-    public function __construct(private readonly CookieProviderInterface $originalService)
+    public function __construct(
+        private readonly CookieProviderInterface $originalService,
+        private readonly SystemConfigService $systemConfigService
+    )
     {
     }
 
     public function getCookieGroups(): array
     {
         $cookieGroups = $this->originalService->getCookieGroups();
+
+        $osmCookieConsent = $this->systemConfigService->get('MoorlFoundation.config.osmCookieConsent');
+        if (!$osmCookieConsent) {
+            return $cookieGroups;
+        }
 
         foreach ($cookieGroups as $groupIndex => $cookieGroup) {
             if ($cookieGroup['snippet_name'] !== 'cookie.groupComfortFeatures') {
