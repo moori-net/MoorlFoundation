896ce197f23b30f1ccf2e1f2cc67dbe92d9ef63a
moorl <info@moorleiche.com>
2023-10-22 20:14:08 +0200

    dies das

--- src/Resources/app/administration/src/component/flow-crm-mapping/index.js
+++ src/Resources/app/administration/src/component/flow-crm-mapping/index.js
@@ -14,10 +14,17 @@ Component.register('moorl-flow-crm-mapping', {
         },
         activeFormFields: {
             type: Array,
-            required: false,
-            default: [],
+            required: true
         },
-        mapping: {
+        formOptions: {
+            type: Array,
+            required: true
+        },
+        config: {
+            type: Object,
+            required: true
+        },
+        clientCriteria: {
             type: Object,
             required: true
         },
@@ -28,6 +35,48 @@ Component.register('moorl-flow-crm-mapping', {
             return this.$tc('moorl-flow-crm-mapping.description', 0, {
                 name: this.name
             });
+        },
+
+        propertiesClientId() {
+            return this.$tc('moorl-flow-crm-mapping.properties.clientId', 0, {
+                name: this.name
+            });
+        },
+
+        helpTextClientId() {
+            return this.$tc('moorl-flow-crm-mapping.helpText.clientId', 0, {
+                name: this.name
+            });
+        },
+
+        propertiesFormId() {
+            return this.$tc('moorl-flow-crm-mapping.properties.formId', 0, {
+                name: this.name
+            });
+        },
+
+        helpTextFormId() {
+            return this.$tc('moorl-flow-crm-mapping.helpText.formId', 0, {
+                name: this.name
+            });
+        },
+    },
+
+    methods: {
+        onChangeClient() {
+            this.$emit('change-client');
+        },
+
+        onChangeForm() {
+            this.config.mapping = {};
+
+            this.$emit('change-form');
+        },
+
+        onChangeMapping() {
+            console.log(this.config);
+
+            this.$emit('change-mapping');
         }
     }
 });
