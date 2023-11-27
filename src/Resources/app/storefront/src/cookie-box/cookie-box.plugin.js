6723d14588871501c885fae79d480fa3aa3ec679
moorl <info@moorleiche.com>
2023-11-27 17:49:42 +0100

    cookie box

--- src/Resources/app/storefront/src/cookie-box/cookie-box.plugin.js
+++ src/Resources/app/storefront/src/cookie-box/cookie-box.plugin.js
@@ -5,28 +5,44 @@ import CookieStorageHelper from 'src/helper/storage/cookie-storage.helper';
 export default class MoorlCookieBoxPlugin extends Plugin {
     static options = {
         cookieKey: null,
-        content: null
+        content: null,
+        reload: true,
     };
 
     init() {
-        this._acceptButton = this.el.querySelector('button');
+        this._acceptButton = this.el.querySelector('.js-moorl-cookie-box-button button');
         this._registerEvents();
+        this._toggle();
+    }
 
+    _registerEvents() {
+        document.$emitter.subscribe(COOKIE_CONFIGURATION_UPDATE, (updatedCookies) => {
+            this._toggle();
+        });
+
+        this._acceptButton.addEventListener('click', event => {
+            CookieStorageHelper.setItem(this.options.cookieKey, "1", "30");
+            if (this.options.reload) {
+                window.location.reload();
+                return;
+            }
+
+            const updatedCookies = {"${this.options.cookieKey}": true}
+            document.$emitter.publish(COOKIE_CONFIGURATION_UPDATE, updatedCookies);
+
+            this.el.style.display = "none";
+        });
+    }
+
+    _toggle() {
         if (!CookieStorageHelper.getItem(this.options.cookieKey)) {
             this.el.style.display = "flex";
         } else if (this.options.content) {
             this.el.parentElement.innerHTML = this.options.content;
 
             window.PluginManager.initializePlugins();
+        } else {
+            this.el.style.display = "none";
         }
-    }
-
-    _registerEvents() {
-        /*document.$emitter.subscribe(COOKIE_CONFIGURATION_UPDATE, (updatedCookies) => {});*/
-
-        this._acceptButton.addEventListener('click', event => {
-            CookieStorageHelper.setItem(this.options.cookieKey, "1");
-            window.location.reload();
-        });
     }
 }
