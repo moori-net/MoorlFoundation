965c963e433353f08b1f66ff75524ccffa684ae3
moorl <info@moorleiche.com>
2024-02-13 10:22:27 +0100

    6.6 stuff

--- src/Resources/app/storefront/build/webpack.config.js
+++ src/Resources/app/storefront/build/webpack.config.js
@@ -3,11 +3,11 @@ module.exports = function (params) {
     return {
         resolve: {
             modules: [
-                `${params.basePath}Resources/app/storefront/node_modules/`,
-                `${params.basePath}Resources/app/administration/node_modules/`
+                `${params.basePath}/Resources/app/storefront/node_modules/`,
+                `${params.basePath}/Resources/app/administration/node_modules/`
             ],
             alias: {
-                MoorlFoundation: `${params.basePath}Resources/app/storefront/src/`,
+                MoorlFoundation: `${params.basePath}/Resources/app/storefront/src/`,
             }
         }
     };
