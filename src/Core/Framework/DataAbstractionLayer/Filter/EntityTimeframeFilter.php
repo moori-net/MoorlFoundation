268c1df0e2ba119e0740e7f62b92c7b894ad6582
moorl <info@moorleiche.com>
2024-05-15 21:28:21 +0200

    auto delete stuff

--- src/Core/Framework/DataAbstractionLayer/Filter/EntityTimeframeFilter.php
+++ src/Core/Framework/DataAbstractionLayer/Filter/EntityTimeframeFilter.php
@@ -11,51 +11,35 @@ class EntityTimeframeFilter extends MultiFilter
     public const DATE_FORMAT = 'date_format';
     public const SHOW_BEFORE = 'show_before';
     public const SHOW_AFTER = 'show_after';
+    public const SHOW_FROM = 'show_from';
+    public const SHOW_UNTIL = 'show_until';
     public const TIMEZONE = 'timezone';
 
     public function __construct(string $prefix, array $config = [])
     {
         $time = new \DateTime();
-
-        if (isset($config[self::DATE_FORMAT])) {
-            $format = $config[self::DATE_FORMAT];
-        } else {
-            $format = 'Y-m-d H:i:s';
-        }
-
-        if (isset($config[self::TIMEZONE])) {
-            $time = $time->setTimezone(new \DateTimeZone($config[self::TIMEZONE]));
-        } else {
-            $time = $time->setTimezone(new \DateTimeZone('UTC'));
-        }
-
-        if (isset($config[self::SHOW_BEFORE])) {
-            $showFrom = $time->modify('+' . $config[self::SHOW_BEFORE] . ' hours');
-        } else {
-            $showFrom = $time;
-        }
-
-        if (isset($config[self::SHOW_AFTER])) {
-            $showUntil = $time->modify('-' . $config[self::SHOW_AFTER] . ' hours');
-        } else {
-            $showUntil = $time;
-        }
+        $time = $time->setTimezone(new \DateTimeZone($config[self::TIMEZONE] ?? 'UTC'));
+        $format = $config[self::DATE_FORMAT] ?? 'Y-m-d H:i:s';
+        $showFrom = isset($config[self::SHOW_BEFORE]) ? $time->modify('+' . $config[self::SHOW_BEFORE] . ' hours') : $time;
+        $showUntil = isset($config[self::SHOW_AFTER]) ? $time->modify('-' . $config[self::SHOW_AFTER] . ' hours') : $time;
+        $showFromField = $prefix . '.' . ($config[self::SHOW_FROM] ?? 'showFrom');
+        $showUntilField = $prefix . '.' . ($config[self::SHOW_UNTIL] ?? 'showUntil');
 
         parent::__construct(
             self::CONNECTION_AND,
             [
                 new MultiFilter(
                     MultiFilter::CONNECTION_OR, [
-                        new EqualsFilter($prefix . '.showFrom', null),
-                        new RangeFilter($prefix . '.showFrom', [
+                        new EqualsFilter($showFromField, null),
+                        new RangeFilter($showFromField, [
                             RangeFilter::LTE => $showFrom->format($format)
                         ])
                     ]
                 ),
                 new MultiFilter(
                     MultiFilter::CONNECTION_OR, [
-                        new EqualsFilter($prefix . '.showUntil', null),
-                        new RangeFilter($prefix . '.showUntil', [
+                        new EqualsFilter($showUntilField, null),
+                        new RangeFilter($showUntilField, [
                             RangeFilter::GTE => $showUntil->format($format)
                         ])
                     ]
