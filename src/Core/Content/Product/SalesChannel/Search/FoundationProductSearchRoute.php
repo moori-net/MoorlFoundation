a3ec28a8c633cda1aefc20cbf3eadde6406d6a94
moorl <info@moorleiche.com>
2024-05-16 12:34:51 +0200

    fix search disable

--- src/Core/Content/Product/SalesChannel/Search/FoundationProductSearchRoute.php
+++ src/Core/Content/Product/SalesChannel/Search/FoundationProductSearchRoute.php
@@ -9,12 +9,18 @@ use Shopware\Core\Content\Product\SalesChannel\Search\AbstractProductSearchRoute
 use Shopware\Core\Content\Product\SalesChannel\Search\ProductSearchRouteResponse;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class FoundationProductSearchRoute extends AbstractProductSearchRoute
 {
-    public function __construct(private readonly AbstractProductSearchRoute $decorated, private readonly EntitySearchService $searchService, private readonly EventDispatcherInterface $dispatcher)
+    public function __construct(
+        private readonly AbstractProductSearchRoute $decorated,
+        private readonly EntitySearchService $searchService,
+        private readonly EventDispatcherInterface $dispatcher,
+        private readonly SystemConfigService $systemConfigService
+    )
     {
     }
 
@@ -25,6 +31,10 @@ class FoundationProductSearchRoute extends AbstractProductSearchRoute
 
     public function load(Request $request, SalesChannelContext $context, Criteria $criteria): ProductSearchRouteResponse
     {
+        if (!$this->systemConfigService->get('MoorlFoundation.config.advancedSearchActive', $context->getSalesChannelId())) {
+            return $this->decorated->load($request, $context, $criteria);
+        }
+
         $entityListing = $this->searchService->getEntityListing($request, $context->getContext());
         if ($entityListing && $entityListing->getEntityName() !== SalesChannelProductDefinition::ENTITY_NAME) {
             $entityListing->setEventDispatcher($this->dispatcher);
