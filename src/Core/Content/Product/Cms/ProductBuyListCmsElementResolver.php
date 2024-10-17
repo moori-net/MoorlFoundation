91183a1217875682d92aee2cc255b1652f25f2fe
moorl <info@moorleiche.com>
2024-10-17 15:12:43 +0200

    restore variant switch in buy list

--- src/Core/Content/Product/Cms/ProductBuyListCmsElementResolver.php
+++ src/Core/Content/Product/Cms/ProductBuyListCmsElementResolver.php
@@ -2,12 +2,49 @@
 
 namespace MoorlFoundation\Core\Content\Product\Cms;
 
+use Shopware\Core\Content\Cms\Aggregate\CmsSlot\CmsSlotEntity;
+use Shopware\Core\Content\Cms\DataResolver\Element\ElementDataCollection;
+use Shopware\Core\Content\Cms\DataResolver\ResolverContext\ResolverContext;
+use Shopware\Core\Content\Cms\SalesChannel\Struct\ProductSliderStruct;
 use Shopware\Core\Content\Product\Cms\ProductSliderCmsElementResolver;
+use Shopware\Core\Content\Product\SalesChannel\Detail\ProductConfiguratorLoader;
+use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity;
+use Shopware\Core\Content\ProductStream\Service\ProductStreamBuilderInterface;
+use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepository;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class ProductBuyListCmsElementResolver extends ProductSliderCmsElementResolver
 {
+    public function __construct(
+        ProductStreamBuilderInterface $productStreamBuilder,
+        SystemConfigService $systemConfigService,
+        SalesChannelRepository $productRepository,
+        private readonly ProductConfiguratorLoader $configuratorLoader
+    ) {
+        parent::__construct($productStreamBuilder, $systemConfigService, $productRepository);
+    }
+
     public function getType(): string
     {
         return 'moorl-product-buy-list';
+    }
+
+    public function enrich(CmsSlotEntity $slot, ResolverContext $resolverContext, ElementDataCollection $result): void
+    {
+        parent::enrich($slot, $resolverContext, $result);
+
+        /** @var ProductSliderStruct $slider */
+        $slider = $slot->getData();
+
+        $products = $slider->getProducts();
+
+        /** @var SalesChannelProductEntity $product */
+        foreach ($products as $product) {
+            if ($product->getSortedProperties() || !$product->getOptionIds()) {
+                continue;
+            }
+
+            $product->setSortedProperties($this->configuratorLoader->load($product, $resolverContext->getSalesChannelContext()));
+        }
     }
 }
