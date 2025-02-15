c2ae79aaec4f1239655f19bad4dc5917fb047287
moorl <info@moorleiche.com>
2025-02-15 13:34:14 +0100

    hotspot image mapping

--- src/Resources/app/storefront/src/hover-card/hover-card.plugin.js
+++ src/Resources/app/storefront/src/hover-card/hover-card.plugin.js
@@ -2,39 +2,73 @@ import Plugin from 'src/plugin-system/plugin.class';
 
 export default class MoorlHoverCardPlugin extends Plugin {
     init() {
-        this._registerEvents();
         this._popover = new bootstrap.Popover(this.el, {
             html: true,
             sanitize: false,
             content: "",
             placement: 'top'
         });
+        this._loaded = false;
+        this._scheduled = null;
+        this._isMouseOverPopover = false;
+        this._registerEvents();
     }
 
     _registerEvents() {
-        let timer;
+        this.el.addEventListener('mouseenter', () => this._showPopover());
+        this.el.addEventListener('mouseleave', () => this._scheduleHidePopover());
+    }
 
-        this.el.addEventListener('mouseenter', () => {
+    _showPopover() {
+        if (this._scheduled) {
+            clearTimeout(this._scheduled);
+            this._scheduled = null;
+            return;
+        }
+
+        if (this._loaded) {
+            this._popover.show();
+            this._attachPopoverEvents();
+        } else {
             fetch(this.el.dataset.moorlHoverCard)
-                .then(body => body.text())
+                .then(response => response.text())
                 .then(body => {
-                    this._popover.content = body;
+                    this._popover.setContent({'.popover-body': body});
                     this._popover.show();
-
-                    this._popover.element.addEventListener('mouseenter', () => {
-                        clearTimeout(timer);
-                    });
-
-                    this._popover.element.addEventListener('mouseleave', () => {
-                        this._popover.hide();
-                    });
+                    this._loaded = true;
+                    this._attachPopoverEvents();
                 });
-        });
+        }
+    }
 
-        this.el.addEventListener('mouseleave', () => {
-            timer = setTimeout(() => {
+    _scheduleHidePopover() {
+        if (this._scheduled) return;
+
+        this._scheduled = setTimeout(() => {
+            if (!this._isMouseOverPopover) {
                 this._popover.hide();
-            }, 600)
-        });
+                this._scheduled = null;
+            }
+        }, 200);
+    }
+
+    _attachPopoverEvents() {
+        window.PluginManager.initializePlugins();
+
+        setTimeout(() => {
+            const popoverElement = this._popover.tip;
+            if (!popoverElement) return;
+
+            popoverElement.addEventListener('mouseenter', () => {
+                clearTimeout(this._scheduled);
+                this._scheduled = null;
+                this._isMouseOverPopover = true;
+            });
+
+            popoverElement.addEventListener('mouseleave', () => {
+                this._isMouseOverPopover = false;
+                this._scheduleHidePopover();
+            });
+        }, 50);
     }
 }
