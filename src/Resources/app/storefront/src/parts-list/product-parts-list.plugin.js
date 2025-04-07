41858d6e246bc442dcdfef9fef9a888ff13cf929
moorl <info@moorleiche.com>
2025-04-07 11:44:29 +0200

    cleanup namespaces

---
+++ src/Resources/app/storefront/src/parts-list/product-parts-list.plugin.js
@@ -0,0 +1,9 @@
+import Plugin from 'src/plugin-system/plugin.class';
+
+export default class MoorlProductPartsListPlugin extends Plugin {
+    static options = {};
+
+    init() {}
+
+    _registerEvents() {}
+}
