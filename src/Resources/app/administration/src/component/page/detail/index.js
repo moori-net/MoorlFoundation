3f5a5b23b7cb53d328bb0c1d54f4cae88bbf8850
moorl <info@moorleiche.com>
2025-04-18 19:11:45 +0200

    abstract pages

--- src/Resources/app/administration/src/component/page/detail/index.js
+++ src/Resources/app/administration/src/component/page/detail/index.js
@@ -16,66 +16,68 @@ const order = [
 
 const mapping = {
     // Contact
-    title: {tab: 'general', card: 'contact'},
-    firstName: {tab: 'general', card: 'contact'},
-    lastName: {tab: 'general', card: 'contact'},
-    salutation: {tab: 'general', card: 'contact', attributes: {labelProperty: 'displayName'}},
-    phoneNumber: {tab: 'general', card: 'contact',},
-    shopUrl: {tab: 'general', card: 'contact',},
-    merchantUrl: {tab: 'general', card: 'contact',},
-    email: {tab: 'general', card: 'contact',},
+    salutation: {order: 0, tab: 'general', card: 'contact', attributes: {labelProperty: 'displayName'}},
+    salutationId: {order: 0},
+    title: {order: 10, tab: 'general', card: 'contact'},
+    firstName: {order: 20, tab: 'general', card: 'contact'},
+    lastName: {order: 30, tab: 'general', card: 'contact'},
+    phoneNumber: {order: 50, tab: 'general', card: 'contact',},
+    shopUrl: {order: 60, tab: 'general', card: 'contact',},
+    merchantUrl: {order: 70, tab: 'general', card: 'contact',},
+    email: {order: 80, tab: 'general', card: 'contact',},
 
-    name: {tab: 'general', card: 'general',},
-    teaser: {tab: 'general', card: 'general',},
-    description: {tab: 'general', card: 'general'},
-    descriptionHtml: {tab: 'general', card: 'general'},
-    keywords: {tab: 'general', card: 'general',},
-    type: {tab: 'general', card: 'general',},
+    name: {order: 90, tab: 'general', card: 'general',},
+    teaser: {order: 100, tab: 'general', card: 'general',},
+    description: {order: 110, tab: 'general', card: 'general'},
+    descriptionHtml: {order: 120, tab: 'general', card: 'general'},
+    keywords: {order: 130, tab: 'general', card: 'general',},
+    type: {order: 140, tab: 'general', card: 'general',},
     // Seo / Meta
-    metaTitle: {tab: 'seo', card: 'general'},
-    metaDescription: {tab: 'seo', card: 'general'},
-    metaKeywords: {tab: 'seo', card: 'general'},
-    seoUrls: {tab: 'seo', componentName: 'sw-seo-url'},
+    metaTitle: {order: 150, tab: 'seo', card: 'general'},
+    metaDescription: {order: 160, tab: 'seo', card: 'general'},
+    metaKeywords: {order: 170, tab: 'seo', card: 'general'},
+    seoUrls: {order: 180, tab: 'seo', componentName: 'sw-seo-url'},
     // CMS Page
-    cmsPage: {tab: 'cmsPage', card: 'cmsPage',},
-    fieldConfig: {tab: 'cmsPage', card: 'cmsPage',},
+    cmsPage: {order: 190, tab: 'cmsPage', card: 'cmsPage',},
+    fieldConfig: {order: 200, tab: 'cmsPage', card: 'cmsPage',},
     // visibility
-    active: {tab: 'general', card: 'visibility',},
-    priority: {tab: 'general', card: 'visibility',},
-    category: {tab: 'general', card: 'visibility',},
-    categories: {tab: 'general', card: 'visibility',},
-    salesChannel: {tab: 'general', card: 'visibility',},
-    salesChannels: {tab: 'general', card: 'visibility',},
+    active: {order: 210, tab: 'general', card: 'visibility',},
+    priority: {order: 220, tab: 'general', card: 'visibility',},
+    category: {order: 230, tab: 'general', card: 'visibility',},
+    categories: {order: 240, tab: 'general', card: 'visibility',},
+    salesChannel: {order: 250, tab: 'general', card: 'visibility',},
+    salesChannels: {order: 260, tab: 'general', card: 'visibility',},
     // address
-    street: {tab: 'address', card: 'general',},
-    streetNumber: {tab: 'address', card: 'general',},
-    zipcode: {tab: 'address', card: 'general',},
-    city: {tab: 'address', card: 'general',},
-    countryCode: {tab: 'address', card: 'general',},
-    country: {tab: 'address', card: 'general',},
-    countryState: {tab: 'address', card: 'general',},
-    additionalAddressLine1: {tab: 'address', card: 'general',},
-    additionalAddressLine2: {tab: 'address', card: 'general',},
-    locationPlaceId: {tab: 'address', card: 'general',},
+    street: {order: 270, tab: 'address', card: 'general',},
+    streetNumber: {order: 280, tab: 'address', card: 'general',},
+    zipcode: {order: 290, tab: 'address', card: 'general',},
+    city: {order: 300, tab: 'address', card: 'general',},
+    countryCode: {order: 310, tab: 'address', card: 'general',},
+    country: {order: 320, tab: 'address', card: 'general',},
+    countryState: {order: 330, tab: 'address', card: 'general',},
+    additionalAddressLine1: {order: 340, tab: 'address', card: 'general',},
+    additionalAddressLine2: {order: 350, tab: 'address', card: 'general',},
+    locationPlaceId: {order: 360, tab: 'address', card: 'general',},
     // company
-    company: {tab: 'general', card: 'company',},
-    department: {tab: 'general', card: 'company',},
-    executiveDirector: {tab: 'general', card: 'company',},
-    placeOfFulfillment: {tab: 'general', card: 'company',},
-    placeOfJurisdiction: {tab: 'general', card: 'company',},
-    bankBic: {tab: 'general', card: 'company',},
-    bankIban: {tab: 'general', card: 'company',},
-    bankName: {tab: 'general', card: 'company',},
-    taxOffice: {tab: 'general', card: 'company',},
-    taxNumber: {tab: 'general', card: 'company',},
-    vatId: {tab: 'general', card: 'company',},
+    company: {order: 370, tab: 'general', card: 'company',},
+    department: {order: 380, tab: 'general', card: 'company',},
+    executiveDirector: {order: 390, tab: 'general', card: 'company',},
+    placeOfFulfillment: {order: 400, tab: 'general', card: 'company',},
+    placeOfJurisdiction: {order: 410, tab: 'general', card: 'company',},
+    bankBic: {order: 420, tab: 'general', card: 'company',},
+    bankIban: {order: 430, tab: 'general', card: 'company',},
+    bankName: {order: 440, tab: 'general', card: 'company',},
+    taxOffice: {order: 450, tab: 'general', card: 'company',},
+    taxNumber: {order: 460, tab: 'general', card: 'company',},
+    vatId: {order: 470, tab: 'general', card: 'company',},
     // location
-    locationLat: {tab: 'address', card: 'location',},
-    locationLon: {tab: 'address', card: 'location',},
-    autoLocation: {tab: 'address', card: 'location',},
-    marker: {tab: 'address', card: 'location',},
+    locationLat: {order: 480, tab: 'address', card: 'location',},
+    locationLon: {order: 490, tab: 'address', card: 'location',},
+    autoLocation: {order: 500, tab: 'address', card: 'location',},
+    marker: {order: 510, tab: 'address', card: 'location',},
     // time
     timeZone: {
+        order: 520,
         tab: 'time',
         card: 'general',
         componentName: 'moorl-select-field',
@@ -83,8 +85,8 @@ const mapping = {
             set: 'timeZone'
         }
     },
-    openingHours: {tab: 'time', card: 'general', componentName: 'moorl-opening-hours'},
-    showOpeningHours: {tab: 'time', card: 'general'},
+    openingHours: {order: 530, tab: 'time', card: 'general', componentName: 'moorl-opening-hours'},
+    showOpeningHours: {order: 540, tab: 'time', card: 'general'},
 };
 
 Shopware.Component.register('moorl-page-detail', {
@@ -118,7 +120,9 @@ Shopware.Component.register('moorl-page-detail', {
 
     data() {
         return {
-            pageStruct: {},
+            pageStruct: {
+                tabs: []
+            },
             snippetStruct: {}
         };
     },
@@ -302,86 +306,63 @@ Shopware.Component.register('moorl-page-detail', {
         },
 
         addColumnToPageStruct(column) {
-            if (this.pageStruct[column.tab] === undefined) {
-                this.pageStruct[column.tab] = {
+            // Tab suchen oder anlegen
+            let tab = this.pageStruct.tabs.find(t => t.id === column.tab);
+            if (!tab) {
+                tab = {
+                    id: column.tab,
                     label: this.getLabel('tab', column.tab),
-                    conditions: [],
-                    cards: {}
+                    cards: []
                 };
+                this.pageStruct.tabs.push(tab);
             }
 
-            if (this.pageStruct[column.tab].cards[column.card] === undefined) {
-                this.pageStruct[column.tab].cards[column.card] = {
+            // Card suchen oder anlegen
+            let card = tab.cards.find(c => c.id === column.card);
+            if (!card) {
+                card = {
+                    id: column.card,
                     label: this.getLabel('card', column.card),
-                    conditions: [],
-                    fields: {}
+                    fields: []
                 };
+                tab.cards.push(card);
             }
 
-            this.pageStruct[column.tab].cards[column.card].fields[column.name] = column;
+            // Field hinzufügen
+            card.fields.push(column);
         },
 
         sortPageStruct() {
-            const sortedStruct = {};
+            const mergedMapping = Object.assign({}, mapping, this.customMapping);
 
-            const tabOrder = [...order, ...Object.keys(this.pageStruct).filter(t => !order.includes(t))];
-
-            const byOrder = (a, b) => {
-                const aIndex = order.indexOf(a);
-                const bIndex = order.indexOf(b);
-                if (aIndex === -1 && bIndex === -1) return a.localeCompare(b);
-                if (aIndex === -1) return 1;
-                if (bIndex === -1) return -1;
-                return aIndex - bIndex;
+            const getOrderIndex = (id) => {
+                const index = order.indexOf(id);
+                return index === -1 ? 9999 : index;
             };
 
-            const sortFields = (fieldKeys, currentCard, currentTab) => {
-                return fieldKeys.sort((a, b) => {
-                    const aInMap = mapping[a];
-                    const bInMap = mapping[b];
+            // Tabs sortieren
+            this.pageStruct.tabs.sort((a, b) => getOrderIndex(a.id) - getOrderIndex(b.id));
 
-                    const aValid = aInMap && aInMap.card === currentCard && aInMap.tab === currentTab;
-                    const bValid = bInMap && bInMap.card === currentCard && bInMap.tab === currentTab;
+            this.pageStruct.tabs.forEach(tab => {
+                // Cards sortieren
+                tab.cards.sort((a, b) => getOrderIndex(a.id) - getOrderIndex(b.id));
 
-                    if (aValid && bValid) {
-                        return Object.keys(mapping).indexOf(a) - Object.keys(mapping).indexOf(b);
-                    }
+                tab.cards.forEach(card => {
+                    // Fields sortieren nach "order" aus mergedMapping
+                    card.fields.sort((a, b) => {
+                        const aCfg = mergedMapping[a.name];
+                        const bCfg = mergedMapping[b.name];
 
-                    return a.localeCompare(b);
-                });
-            };
+                        if (aCfg?.order !== undefined && bCfg?.order !== undefined) {
+                            return aCfg.order - bCfg.order;
+                        }
+                        if (aCfg?.order !== undefined) return -1;
+                        if (bCfg?.order !== undefined) return 1;
 
-            tabOrder.forEach(tabKey => {
-                const tab = this.pageStruct[tabKey];
-                if (!tab) return;
-
-                const cardKeys = Object.keys(tab.cards).sort(byOrder);
-                const sortedCards = {};
-
-                cardKeys.forEach(cardKey => {
-                    const card = tab.cards[cardKey];
-
-                    const fieldKeys = Object.keys(card.fields);
-                    const sortedFieldKeys = sortFields(fieldKeys, cardKey, tabKey);
-
-                    const sortedFields = {};
-                    sortedFieldKeys.forEach(fieldKey => {
-                        sortedFields[fieldKey] = card.fields[fieldKey];
+                        return a.name.localeCompare(b.name);
                     });
-
-                    sortedCards[cardKey] = {
-                        ...card,
-                        fields: sortedFields
-                    };
                 });
-
-                sortedStruct[tabKey] = {
-                    ...tab,
-                    cards: sortedCards
-                };
             });
-
-            this.pageStruct = sortedStruct;
         }
     }
 });
