f5c902b421dd97ac1fb3ecf811b2ad9e4d848447
moorl <info@moorleiche.com>
2025-04-18 14:03:29 +0200

    abstract pages

--- src/Resources/app/administration/src/component/page/detail/index.js
+++ src/Resources/app/administration/src/component/page/detail/index.js
@@ -12,20 +12,62 @@ const order = [
 ];
 
 const mapping = {
-    active: {tab: 'general', card: 'visibility',},
     name: {tab: 'general', card: 'general',},
+    teaser: {tab: 'general', card: 'general',},
     description: {tab: 'general', card: 'general'},
+    descriptionHtml: {tab: 'general', card: 'general'},
     keywords: {tab: 'general', card: 'general',},
-    seoUrls: {
-        tab: 'seo',
-        componentName: 'sw-seo-url',
-    },
+    type: {tab: 'general', card: 'general',},
+
+    seoUrls: {tab: 'seo', componentName: 'sw-seo-url'},
     cmsPage: {tab: 'cmsPage', card: 'cmsPage',},
+
+    active: {tab: 'general', card: 'visibility',},
+    priority: {tab: 'general', card: 'visibility',},
     category: {tab: 'general', card: 'visibility',},
     categories: {tab: 'general', card: 'visibility',},
     salesChannel: {tab: 'general', card: 'visibility',},
     salesChannels: {tab: 'general', card: 'visibility',},
-    salutation: {tab: 'general', card: 'contact', labelProperty: 'displayName',}
+
+    salutation: {tab: 'general', card: 'contact', labelProperty: 'displayName',},
+    title: {tab: 'general', card: 'contact',},
+    firstName: {tab: 'general', card: 'contact',},
+    lastName: {tab: 'general', card: 'contact',},
+    email: {tab: 'general', card: 'contact',},
+    phoneNumber: {tab: 'general', card: 'contact',},
+    shopUrl: {tab: 'general', card: 'contact',},
+    merchantUrl: {tab: 'general', card: 'contact',},
+
+    street: {tab: 'address', card: 'general',},
+    streetNumber: {tab: 'address', card: 'general',},
+    zipcode: {tab: 'address', card: 'general',},
+    city: {tab: 'address', card: 'general',},
+    countryCode: {tab: 'address', card: 'general',},
+    country: {tab: 'address', card: 'general',},
+    countryState: {tab: 'address', card: 'general',},
+    additionalAddressLine1: {tab: 'address', card: 'general',},
+    additionalAddressLine2: {tab: 'address', card: 'general',},
+    locationPlaceId: {tab: 'address', card: 'general',},
+
+    company: {tab: 'general', card: 'company',},
+    department: {tab: 'general', card: 'company',},
+    executiveDirector: {tab: 'general', card: 'company',},
+    placeOfFulfillment: {tab: 'general', card: 'company',},
+    placeOfJurisdiction: {tab: 'general', card: 'company',},
+    bankBic: {tab: 'general', card: 'company',},
+    bankIban: {tab: 'general', card: 'company',},
+    bankName: {tab: 'general', card: 'company',},
+    taxOffice: {tab: 'general', card: 'company',},
+    taxNumber: {tab: 'general', card: 'company',},
+    vatId: {tab: 'general', card: 'company',},
+
+    locationLat: {tab: 'address', card: 'location',},
+    locationLon: {tab: 'address', card: 'location',},
+    autoLocation: {tab: 'address', card: 'location',},
+    marker: {tab: 'address', card: 'location',},
+
+    timeZone: {tab: 'time', card: 'general',},
+    openingHours: {tab: 'time', card: 'general',},
 };
 
 const customMapping = {
@@ -146,8 +188,10 @@ Shopware.Component.register('moorl-page-detail', {
                         column.model = 'entityCollection';
                         column.componentName = 'sw-entity-many-to-many-select';
 
-                        column.attrs.entity = field.entity;
                         column.attrs.labelProperty = field.flags.moorl_label_property ?? 'name';
+
+                        console.log('sw-entity-many-to-many-select');
+                        console.log(column);
                     }
                 }
 
@@ -181,6 +225,7 @@ Shopware.Component.register('moorl-page-detail', {
                 }
 
                 column.attrs.label = column.label;
+                column.attrs.componentName = column.componentName;
 
                 this.addColumnToPageStruct(column);
             }
