c98937e57b3ad4e1cc91ff080ee8a076613803e5
moorl <info@moorleiche.com>
2025-06-04 16:46:07 +0200

    remove second entry from accordion

--- src/Resources/app/administration/src/module/sw-cms/elements/accordion/index.js
+++ src/Resources/app/administration/src/module/sw-cms/elements/accordion/index.js
@@ -22,13 +22,7 @@ Shopware.Application.getContainer('service').cmsService.registerCmsElement({
                     name: 'This is my first entry',
                     content:
                         '<p>Lorem ipsum dolor sit amet, <a href="#">consetetur</a> sadipscing elitr, <b>sed diam nonumy eirmod tempor</b> invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua</p>',
-                },
-                {
-                    id: 2,
-                    name: 'This is my second entry',
-                    content:
-                        '<p>Lorem ipsum dolor sit amet, <a href="#">consetetur</a> sadipscing elitr, <b>sed diam nonumy eirmod tempor</b> invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua</p>',
-                },
+                }
             ],
         },
         autoClose: {
