a4be189d1e648d70c2ea1bab4d6df10b39a3ea71
moorl <info@moorleiche.com>
2025-08-14 13:44:38 +0200

    fix storefront modal

--- src/Resources/app/storefront/src/modal/modal.plugin.js
+++ src/Resources/app/storefront/src/modal/modal.plugin.js
@@ -1,7 +1,10 @@
 const Plugin = window.PluginBaseClass;
+import HttpClient from 'src/service/http-client.service';
 
 export default class MoorlModal extends Plugin {
     init() {
+        this._client = new HttpClient(window.accessKey, window.contextToken);
+
         this._registerEvents();
     }
 
@@ -10,7 +13,30 @@ export default class MoorlModal extends Plugin {
             const url = this.el.dataset.moorlModal;
             if (!url) {return;}
             evt.preventDefault();
-            window.moorlFoundationModal(url);
+            this._openModal(url);
+        });
+    }
+
+    _openModal(url, callback) {
+        this._client.get(url, (response) => {
+            const modal = document.getElementById('moorlFoundationModal');
+            if (!modal) {
+                console.error(`#moorlFoundationModal not found. Please be sure, that views/storefront/base.html.twig is extended correctly`)
+            }
+
+            modal.innerHTML = response;
+
+            const bsModal =
+                bootstrap.Modal.getInstance(modal) ?? new bootstrap.Modal(modal);
+            bsModal.show();
+
+            modal.addEventListener('hidden.bs.modal', () => {
+                modal.innerHTML = '';
+            });
+
+            if (typeof callback == 'function') {
+                callback(modal, bsModal);
+            }
         });
     }
 }
